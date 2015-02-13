5265dc0ea56f80034c4f7c97dbde02f712fb6aae
Nicolas Jaouen <nijaouen@bouyguestelecom.fr>
2015-02-13 11:18:01 +0100

    version 2.0.3 de la lib

---
+++ RemoteTest/Constant.h
@@ -0,0 +1,27 @@
+//
+//  Constant.h
+//  RemoteTest
+//
+//  Created by Nicolas Jaouen on 29/01/2015.
+//  Copyright (c) 2015 Bouygues Telecom. All rights reserved.
+//
+
+
+//Application and developper code 
+#define Button_name @[ @"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"0",@"UP",@"DOWN",@"LEFT",@"RIGHT",@"OK",@"BACK",@"EXIT"]
+
+//BBox api key
+#define BBoxIp @"bboxIp"
+#define BBoxControler @"BboxControler"
+#define Application_ApiKey @"Application"
+
+// Key
+#define Body_KEY @"body"
+#define State_KEY @"state"
+#define AppName_KEY @"appName"
+
+//Type
+#define Keypressed_Type @"keypressed"
+
+//Identifier
+#define Cell_Identifier @"Cell"
